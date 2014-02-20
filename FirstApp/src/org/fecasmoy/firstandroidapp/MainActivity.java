870e1fd025b1b7ccb789f3e22d2c1401b1834e09
fecasmoy <fecasmoy@gmail.com>
2014-02-21 00:42:42 +0800

    a

--- FirstApp/src/org/fecasmoy/firstandroidapp/MainActivity.java
+++ FirstApp/src/org/fecasmoy/firstandroidapp/MainActivity.java
@@ -1,10 +1,17 @@
 package org.fecasmoy.firstandroidapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	public final static String EXTRA_MESSAGE = "org.fecasmoy.firstandroidapp.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,26 @@ public class MainActivity extends Activity {
 		System.out.println("helloworld.");
 		System.out.println("Test from the server.");
 		return true;
+	}
+
+	/**
+	 * Called when the user clicks the Send button
+	 */
+	public void sendMessage(View view) {
+		// Do something in response to button
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
+
+	public void showToast(CharSequence text) {
+		Context context = getApplicationContext();
+		int duration = Toast.LENGTH_SHORT;
+
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
 	}
 
 }
